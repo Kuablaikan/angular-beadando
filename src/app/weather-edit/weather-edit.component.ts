90add0f417212e1855b35eded0fa1567d8746d68
Tóvári Tamás <kuablaikan@hotmail.com>
2021-05-25 17:17:13 +0200

    validation added to WeatherEditComponent

--- src/app/weather-edit/weather-edit.component.ts
+++ src/app/weather-edit/weather-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from "@angular/router";
-import { FormControl, FormGroup } from "@angular/forms";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 import { Weather } from "../weather";
 import { WeatherProviderService } from "../weather-provider.service";
@@ -13,31 +13,41 @@ import { WeatherProviderService } from "../weather-provider.service";
 export class WeatherEditComponent {
 
   weatherForm = new FormGroup({
-    city: new FormControl(''),
-    temperature: new FormControl(''),
-    wind: new FormControl(''),
-    description: new FormControl('')
+    city: new FormControl('', Validators.required),
+    temperature: new FormControl('', Validators.required),
+    wind: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required)
   });
 
   private _weather: Weather = { city: "", temperature: "", wind: "", description: "" };
+  private _isValid: boolean = false;
+
+  showErrorMessage: boolean | undefined = undefined;
 
   constructor(private _weatherProviderService: WeatherProviderService,
               private _router: Router) {
     this._weather = this._weatherProviderService.weather;
+    this.weatherForm.valueChanges.subscribe(() => {
+      this._weather = this.weatherForm.value;
+      this._isValid = this.weatherForm.status !== 'INVALID';
+      if (this.showErrorMessage !== undefined)
+        this.showErrorMessage = this._isValid ? false : true;
+    });
     this.weatherForm.patchValue({
       city: this._weather.city,
       temperature: this._weather.temperature,
       wind: this._weather.wind,
       description: this._weather.description
     });
-    this.weatherForm.valueChanges.subscribe(() => {
-      this._weather = this.weatherForm.value;
-    });
   }
 
   saveAndNavigateBack(): void {
-    this._weatherProviderService.weather = this._weather;
-    this._router.navigate(['/list']);
+    if (this._isValid) {
+      this._weatherProviderService.weather = this._weather;
+      this._router.navigate(['/list']);
+    } else {
+      this.showErrorMessage = true;
+    }
   }
 
   cancelAndNavigateBack(): void {
